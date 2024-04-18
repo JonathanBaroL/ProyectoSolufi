37ecb2a3e53c2c1450a489b03bc51c647b3d8e24
JonathanBaroL <jonathan.developer21@gmail.com>
2024-04-18 15:35:18 -0600

    sql server

--- backend/external_bdd/urls.py
+++ backend/external_bdd/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
+    path('api/get-vehiculos/', views.get_vehiculos.as_view(), name='get_vehiculos'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),            #
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),           # URLS para acceso a la verificacion del token
 ]
--- backend/external_bdd/views.py
+++ backend/external_bdd/views.py
@@ -9,3 +9,25 @@ from rest_framework.response import Response  # clase de respuesta de la API de 
 from rest_framework_simplejwt.authentication import JWTAuthentication  # Importa la autenticación JWT de Simple JWT
 
 # vista de la API
+
+class get_vehiculos(APIView):
+    # Utiliza el permiso de autenticación, solo los usuarios autenticados pueden acceder a esta vista
+    permission_classes = [IsAuthenticated]  
+    
+    # Define las clases de autenticación que se aplicarán a la vista
+    # Utiliza la autenticación JWT, lo que significa que los tokens JWT se utilizarán para autenticar a los usuarios
+    authentication_classes = [JWTAuthentication]  
+    
+    def get(self, request):
+        # Accede al usuario autenticado a través del objeto request
+        user = request.user
+        print("Acceso correcto a la vista para: " + str(user))
+         # Lógica para obtener los vehículos
+        vehiculos = ListaCarros.objects.all()
+        
+        # Aquí podrías necesitar convertir vehiculos a un formato serializable si no es un diccionario
+        vehiculos_serializados = [{'name': vehiculo.name, 'description': vehiculo.description} for vehiculo in vehiculos]
+
+        # Devolver la respuesta HTTP
+        return JsonResponse({'vehiculos': vehiculos_serializados}, safe=False)
+
